a403cb941a53334ba367b10b5e9cde5b50a5709e
tushar pareek <141040225+mandelboundary@users.noreply.github.com>
2026-08-12 15:39:36 +0530

    Configure static hosting export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
+  distDir: "dist",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
